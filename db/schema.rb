ebb3bad44a4b8e971695a780934c22165294bfb6
EC2 Default User <ec2-user@ip-172-31-44-237.us-east-2.compute.internal>
2019-07-19 07:47:20 +0000

    sqliteからpgに変更

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2019_07_18_012758) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "members", force: :cascade do |t|
     t.string "name"
     t.string "yomi"
